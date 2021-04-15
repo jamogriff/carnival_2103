9049907fa87c58f61abd6b16e9330f2b1288a7b1
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-15 09:46:13 -0600

    Implement recommend_rides with helper mathods

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -11,6 +11,26 @@ class Carnival
   end
 
   def recommend_rides(attendee)
+    included_names = attendee.interests.select do |interest|
+      get_ride_names.include?(interest)
+    end
+    included_names.flat_map do |name|
+      get_ride_from_name(name)
+    end.uniq
+  end
+
+  # NOTE: The following two helper methods
+  # don't have associated tests
+  def get_ride_from_name(name)
+    @rides.select do |ride|
+      ride.name == name
+    end
+  end
+
+  def get_ride_names
+    @rides.map do |ride|
+      ride.name
+    end
   end
 
 end
