6eb68efe35a9e3dce8a25728a30b175184a3dc28
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-15 09:13:07 -0600

    Add interest test

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,9 +1,10 @@
 class Attendee
-  attr_reader :name, :spending_money
+  attr_reader :name, :spending_money, :interests
 
   def initialize(name, age)
     @name = name
     @spending_money = age
+    @interests = []
   end
 
 end
--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -2,7 +2,7 @@ require './lib/attendee'
 
 RSpec.describe Attendee do
 
-  describe 'initialization' do
+  describe 'initialization and interests' do
     attendee = Attendee.new('Bob', 20)
 
     it 'exists' do
@@ -12,6 +12,13 @@ RSpec.describe Attendee do
     it 'accesses its attributes' do
       expect(attendee.name).to eq("Bob")
       expect(attendee.spending_money).to eq(20)
+      expect(attendee.interests).to be_instance_of(Array)
+    end
+
+    it 'can add interests' do
+      attendee.add_interest('Bumper Cars')
+      attendee.add_interest('Ferris Wheel')
+      expect(attendee.interests.length).to eq(2)
     end
   end
 end
