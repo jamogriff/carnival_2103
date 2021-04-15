839cc4810f48c04c75fd9f76c676c7ef6dc2be8e
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-15 09:21:25 -0600

    Add add_rides tests

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -4,7 +4,7 @@ require './lib/carnival'
 
 RSpec.describe Carnival do
 
-  describe 'initialization' do
+  describe 'initialization and rides' do
     jeffco_fair = Carnival.new("Jefferson County Fair")
 
     it 'exists' do
@@ -15,6 +15,16 @@ RSpec.describe Carnival do
       expect(jeffco_fair.name).to eq("Jefferson County Fair")
       expect(jeffco_fair.rides).to be_instance_of(Array)
     end
-    
+
+    it 'can add rides' do
+      ferris_wheel = Ride.new({name: 'Ferris Wheel', cost: 0})
+      bumper_cars = Ride.new({name: 'Bumper Cars', cost: 10})
+      scrambler = Ride.new({name: 'Scrambler', cost: 15})
+      jeffco_fair.add_ride(ferris_wheel)
+      jeffco_fair.add_ride(bumper_cars)
+      jeffco_fair.add_ride(scrambler)
+      expect(jeffco_fair.rides.length).to eq(3)
+    end
+
   end
 end
